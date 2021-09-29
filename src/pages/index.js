40055a4b822ad562ee0dcbb2a600fc285ae0466c
Danny Hollis <dannyhollisdevelopment@gmail.com>
2021-09-29 00:54:58 -0400

    Added a photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle='Homeeeee page'>
       <p>I am making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt='Uncle Danny carrying Emma through a crosswalk.'
+        src='../images/danny-and-emma.jpeg'
+      />
     </Layout>
   );
 };
